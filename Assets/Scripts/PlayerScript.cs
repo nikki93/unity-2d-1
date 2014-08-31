39beb8e5002812165391b8f5ec3a43fcabe00073
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-08-31 02:47:47 -0700

    add PlayerScript script

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(50, 50);
+
+    private Vector3 movement;
+
+    void Update()
+    {
+        movement = new Vector2(speed.x * Input.GetAxis("Horizontal"),
+                               speed.y * Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody2D.velocity = movement;
+    }
+}
