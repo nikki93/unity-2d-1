5277ddef1833c426f52398433f5f9ccb11ae68e9
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-08-31 14:01:04 -0700

    add MoveScript for Poulpi

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(10, 10);
+    public Vector2 direction = new Vector2(-1, 0);
+
+    private Vector2 movement;
+
+    void Start()
+    {
+        movement = new Vector2(speed.x * direction.x,
+                               speed.y * direction.y);
+    }
+
+    void Update()
+    {
+        rigidbody2D.velocity = movement;
+    }
+}
